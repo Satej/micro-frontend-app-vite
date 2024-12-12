895c063ecbcfc2692fabbcf326948df1996628ca
Satej <Satej@users.noreply.github.com>
2024-12-12 00:48:45 +0000

    Init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # micro-frontend-app-vite
+
+[Ref](https://dev.to/mairouche/setup-a-micro-frontend-architecture-in-15min-with-vite-4pbg)
+
+cd news-portal/host/apps/header
+npm i
+npm run build
+
+cd news-portal/host/apps/trending
+npm i
+npm run build
+
+cd news-portal/host/
+npm run dev
+
+Visit app at [http://localhost:3000](http://localhost:3000).
---
+++ news-portal/host/src/main.js
@@ -0,0 +1,7 @@
+import { mount as mountHeader } from '../apps/header/dist/header.js';
+import { mount as mountTrending } from '../apps/trending/dist/trending.js';
+//import { mount as mountHighlights } from '../apps/highlights/dist/highlights.js';
+
+mountHeader(document.querySelector('#header'));
+mountTrending(document.querySelector('#trending'));
+//mountHighlights(document.querySelector('#highlights'));
